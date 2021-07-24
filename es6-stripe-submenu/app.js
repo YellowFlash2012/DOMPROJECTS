b42f4b8cff99c6c140d361d64216ba632c8857b6
YellowFlash2012 <cyclop9@hotmail.com>
2021-07-24 09:21:36 +0100

    index.html style.css app.js

---
+++ es6-stripe-submenu/app.js
@@ -0,0 +1,24 @@
+import sublinks from './data.js'
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar-links');
+
+const linksBtns = [...document.querySelectorAll('.link-btn')];
+console.log(linksBtns);
+
+const submenu = document.querySelector('.submenu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('.nav');
+
+//hide/show sidebar
+toggleBtn.addEventListener('click', () => {
+    sidebarWrapper.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebarWrapper.classList.remove('show');
+});
+
+console.log(sublinks);
